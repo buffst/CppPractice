65ac1d1a6331297106ec1b08e5673fd7ab2fe6cd
buffst <buffaloes.taki@gmail.com>
2022-11-10 04:17:22 +0900

    first

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	int input;
+	cout << "®”’l‚ğ“ü—ÍF";
+	cin >> input;
+	if (input % 2 == 0) {
+		cout << input << "‚Í‹ô”‚Å‚·B" << endl;
+	}
+	return 0;
+}
